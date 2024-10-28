a19d7be1a71c5acdd34d8ba91b67a47e73d0a09a
Xolelwa34 <ciliaswekile@gmail.com>
2024-10-28 16:41:32 +0000

    making a hyper pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Implement a get_hyper method that takes the same arguments (and defaults)
+"""
+
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Returns pagination metadata and the requested page of data."""
+        data = self.get_page(page, page_size)
+
+        total_rows = len(self.dataset())
+        total_pages = math.ceil(total_rows / page_size)
+
+        prev_page = page - 1 if page > 1 else None
+        next_page = page + 1 if page < total_pages else None
+
+        return {
+            "page_size": page_size,
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages,
+        }
