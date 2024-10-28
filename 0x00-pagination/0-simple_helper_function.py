d7a59897d206e26289e72f00845a438546c58bd6
Xolelwa34 <ciliaswekile@gmail.com>
2024-10-28 16:24:57 +0000

    adding page 1 plus size

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,15 +3,11 @@
  index_range function that takes two integer arguments page and page_size
 """
 
-from typing import Tuple
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page, page_size):
     """
-     Method to retreive index_range from page to page_size
-"""
-
- start_index = (page - 1) * page_size
+    Returns: tuple: A tuple containing the
+        start index and end index for the given page.
+    """
+    start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    return (start_index, end_index)
-
+    return start_index, end_index
