05813fc8f9c37726555f024bce3997dd63d4c76a
Xolelwa34 <ciliaswekile@gmail.com>
2024-10-30 07:54:32 +0000

    MRU caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Least Recently Used caching module.
+Least Recently Used caching
 """
 from collections import OrderedDict
 
@@ -9,18 +9,18 @@ from base_caching import BaseCaching
 
 class LRUCache(BaseCaching):
     """
-    Method that retrieves and stores items
+    Method that automates the process of storing function results in a cache
     """
-def __init__(self):
-    """
-     Initializes the cache
-    """
-    super().__init__()
-    self.order = []
-
-def put(self, key, item):
+    def __init__(self):
         """
-         Puts an item in the cache.
+        Initializes the cache.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Puts an item in the cache.
         """
         if key is None or item is None:
             return
@@ -35,9 +35,8 @@ def put(self, key, item):
 
     def get(self, key):
         """
-         Gets an item by key.
+        Gets an item by key.
         """
         if key is not None and key in self.cache_data:
             self.cache_data.move_to_end(key, last=False)
         return self.cache_data.get(key, None)
-
